39de9ac06fd246581c8d2517705fffce4f544572
ama6erry <ahmadalberry06@gmail.com>
2026-09-11 15:04:15 +0300

    Polished UI

---
+++ site.js
@@ -0,0 +1,22 @@
+const header = `
+    <header>
+        <div id="website-title">
+            <a href="index.html">&#xE070;hmad Alberr&#xE0B8;</a>
+        </div> 
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="projects.html">Projects</a></li>
+                <li><a href="blog.html">Blog</a></li>
+                <li><a href="chip8-demo.html">Demo</a></li>
+                <button id="crt-toggle">CRT mode</button>
+            </ul>
+        </nav>
+    </header>
+`
+
+const query = document.querySelector("#site-header");
+
+if(query){
+    query.innerHTML = header;
+}
